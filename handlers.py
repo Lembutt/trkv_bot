74471074482a954c0a053f20d276e38326cec182
Lembutt <comradeyar@gmail.com>
2021-07-21 16:39:17 +0300

    added ripem and hello message on start command

--- handlers.py
+++ handlers.py
@@ -3,6 +3,12 @@ from main import dp, bot
 from aiogram.types import Message
 from market_api import Market
 
+@dp.message_handler(commands=['start'])
+async def hello_message(message: Message):
+    if message.from_user.id not in ADMINS:
+        return 0
+    await message.answer(f'Ну че, здорова, {message.from_user.first_name} {message.from_user.last_name}\n\nЩа к барыгам, короче, а потом по бабам:)')
+
 @dp.message_handler()
 async def get_item_info(message: Message):
     mark = await Market.get_item(message.text)
